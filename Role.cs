01c57bef5c80f151e4650d6f9d821c1a0e822343
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-27 17:46:04 +0100

    Create Role.cs

---
+++ Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace FlyPal.Models
+{
+    public class Role : IdentityRole<int>
+    {
+
+    }
+}
