ffac06d74d412fed282da1aee480d3f656e1f7e5
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-27 18:05:35 +0100

    Create UserAndComplainSearchResult.cs

---
+++ UserAndComplainSearchResult.cs
@@ -0,0 +1,9 @@
+namespace FlyPal.Models.ViewModels
+{
+    public class UserAndComplainSearchResult
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
