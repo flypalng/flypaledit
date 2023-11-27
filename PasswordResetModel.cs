3a80d72d9fa9522ecc572394871895e72c93aca2
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-27 17:52:44 +0100

    Create PasswordResetModel.cs

---
+++ PasswordResetModel.cs
@@ -0,0 +1,8 @@
+namespace FlyPal.Models.InputModels
+{
+    public class PasswordResetModel
+    {
+        public string ExistingPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
