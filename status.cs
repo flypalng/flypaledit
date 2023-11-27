e5fdf7cc202859e8c50aed1bcf026f4ac3d8b87f
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-27 17:45:20 +0100

    Create status.cs

---
+++ status.cs
@@ -0,0 +1,25 @@
+namespace FlyPal.Models.AppModels
+{
+    public class Status : BaseModel
+    {
+        public string Name { get; set; }
+    }
+
+    enum Statuses
+    {
+        PENDING = 1,
+        APPROVED,
+        ONGOING,
+        RESOLVED,
+        VERIFIED,
+        DRAFTED,
+        ACTIVE,
+        INACTIVE,
+        REJECTED,
+        SOLD,
+        COMPLETED,
+        ACCEPTED,
+        REVIEWED,
+        DECLINED
+    }
+}
