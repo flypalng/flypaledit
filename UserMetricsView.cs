bf279d0ec3726a96ca7a86eaf0e4f9cf084e21d7
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-27 18:05:58 +0100

    Create UserMetricsView.cs

---
+++ UserMetricsView.cs
@@ -0,0 +1,9 @@
+namespace FlyPal.Models.ViewModels
+{
+    public class UserMetricsView
+    {
+        public int PendingComplaints { get; set; }
+        public int ResolvedComplaints { get; set; }
+        public int DeclinedComplaints { get; set; }
+    }
+}
