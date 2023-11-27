e8f62d2a4f3e73eff4a4b334136f3927a328207d
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-27 18:04:19 +0100

    Create MetricsView.cs

---
+++ MetricsView.cs
@@ -0,0 +1,9 @@
+namespace FlyPal.Models.ViewModels
+{
+    public class MetricsView
+    {
+        public int NewUsers { get; set; }
+        public int ActiveUsers { get; set; }
+        public int Users { get; set; }
+    }
+}
