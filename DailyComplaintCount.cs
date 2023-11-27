fe0c6ed52163be0661e8b58b684a6e30fcfc5226
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-27 18:02:05 +0100

    Create DailyComplaintCount.cs

---
+++ DailyComplaintCount.cs
@@ -0,0 +1,9 @@
+namespace FlyPal.Models.ViewModels
+{
+    public class DailyComplaintCount
+    {
+        public int Count { get; set; }
+        public int Date { get; set; }
+        public string DateName { get; set; }
+    }
+}
