32458764dc9ac72a3fe9c0ecd7ab48eb5559ab4f
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-27 18:06:22 +0100

    Create UserView.cs

---
+++ UserView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyPal.Models.ViewModels
+{
+    public class UserView
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Token { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ProfilePhotoReference { get; set; }
+        public UserMetricsView Metrics { get; set; }
+
+    }
+}
