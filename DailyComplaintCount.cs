04c73181a6ab8c8fa7cbdf465d324b9fdd2c55d4
flypalng <103930296+flypalng@users.noreply.github.com>
2023-11-28 12:54:26 +0100

    Delete DailyComplaintCount.cs

--- DailyComplaintCount.cs
+++
@@ -1,9 +0,0 @@
-namespace FlyPal.Models.ViewModels
-{
-    public class DailyComplaintCount
-    {
-        public int Count { get; set; }
-        public int Date { get; set; }
-        public string DateName { get; set; }
-    }
-}
